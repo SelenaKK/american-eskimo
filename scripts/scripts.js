9464f65ddc5bb9ea4dac29bd15aa88937f14ff76
Kepei Zhang <zhangkp1@bu.edu>
2023-04-18 15:42:03 -0400

    version1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,25 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+
+/*explore more button*/
+const exploreBtn = document.querySelector('.explore-more-btn');
+const text = document.querySelector('.text');
+
+exploreBtn.addEventListener('click', (e)=>{
+    text.classList.toggle('show-more');
+    if (exploreBtn.innerText === 'Explore More'){
+        exploreBtn.innerText ='Explore Less';
+    } else{
+        exploreBtn.innerText ='Explore More';
+    }
+})
+
+/*star rating*/
+
+function hideUnhide(id){
+    const s1 = document.getElementById(id);
+    if (s1.style.display == "block"){
+        s1.style.display = "none";
+    }else {
+        s1.style.display = "block";
+    }
+}
